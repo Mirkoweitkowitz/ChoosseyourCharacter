1aaaf6e176562efe40d062eaaa312215b80a8e88
Mirko Weitkowitz <mweitkowitz@web.de>
2022-09-14 16:33:01 +0200

    Layout erstellt und erste Code teile Programmiert

--- ChoosseyourCharacter/GewähltViewController.swift
+++ ChoosseyourCharacter/GewähltViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GewähltViewController.swift
 //  ChoosseyourCharacter
 //
 //  Created by Mirko Weitkowitz on 14.09.22.
@@ -7,17 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Gewa_hltViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
-
-    @IBAction func findCharacter(_ sender: UIButton) {
-        
-        performSegue(withIdentifier: "picköpfe", sender: self)
-    }
+  
 }
-
+     
+     
---
+++ ChoosseyourCharacter/KöpfeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  KöpfeViewController.swift
+//  ChoosseyourCharacter
+//
+//  Created by Mirko Weitkowitz on 14.09.22.
+//
+
+import UIKit
+
+class Ko_pfeViewController: UIViewController {
+    
+    
+    @IBOutlet weak var marioIMG: UIImageView!
+    
+    
+    @IBOutlet weak var luigiIMG: UIImageView!
+    
+    
+    @IBOutlet weak var yoshiIMG: UIImageView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+   // performSegue(withIdentifier: "character", sender: self)
+}
